03867c330f12b5dd0fd2c4cfc869d3c21bc1f697
PotcFdk <PotcFdk@gmx.net>
2016-07-22 01:59:16 +0200

    Adapt the example class.

--- example/example.hpp
+++ example/example.hpp
@@ -6,19 +6,9 @@
 
 class ExampleClass : public ToxController
 {
-public:
-    // using ToxController::ToxController;
-    ExampleClass (std::string bootstrap_address,
-                  uint16_t bootstrap_port,
-                  std::string bootstrap_key,
-                  std::string nickname,
-                  std::string status
-                 ) : ToxController (bootstrap_address, bootstrap_port, bootstrap_key, nickname, status)
-    {
-        setupCallbacks();
-    };
+    using ToxController::ToxController;
 
 private:
-    void evtOnRequest (Tox *tox, const uint8_t *public_key, const uint8_t *message, size_t length, void *user_data) override;
-    void evtOnMessage (Tox *tox, uint32_t friend_number, TOX_MESSAGE_TYPE type, const uint8_t *message, size_t length, void *user_data) override;
+    void onRequest (Tox *tox, const uint8_t *public_key, const uint8_t *message, size_t length, void *user_data) override;
+    void onMessage (Tox *tox, uint32_t friend_number, TOX_MESSAGE_TYPE type, const uint8_t *message, size_t length, void *user_data) override;
 };
